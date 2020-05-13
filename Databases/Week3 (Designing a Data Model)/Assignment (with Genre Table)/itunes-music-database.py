e5fa382ff912c432836507ebb97642e784e7b696
Zannatun Nayeem <nayeem123able@gmail.com>
2020-05-13 21:53:38 +0600

    Databases: Week3 completed

--- Databases/Week3 (Designing a Data Model)/Assignment (with Genre Table)/itunes-music-database.py
+++ Databases/Week3 (Designing a Data Model)/Assignment (with Genre Table)/itunes-music-database.py
@@ -10,8 +10,14 @@ cur.executescript('''
 DROP TABLE IF EXISTS Artist;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
+DROP TABLE IF EXISTS Genre;
 
 CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
@@ -27,6 +33,7 @@ CREATE TABLE Track (
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id  INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
@@ -59,26 +66,44 @@ for entry in all: #All dicts of data one by one
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
+    genre = lookup(entry, 'Genre')
 
-    if name is None or artist is None or album is None : #All fields needed
+    if name is None or artist is None or album is None or genre is None : #All fields needed
         continue
 
-    print(name, artist, album, count, rating, length)
+    print(name, artist, album, count, rating, length,genre)
+    #ARTIST
     #Inserting artist name
     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
         VALUES ( ? )''', ( artist, ) )
     #
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0] #Get one row and then first item ie ID
+    #ALBUM
     #Inserting Album name with artist_id foreign key
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
         VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0] #Get one row and then first item ie ID
+    #GENRE
+    #Inserting genre name
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+        VALUES ( ? )''', ( genre, ) )    
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0] #Get one row and then first item ie ID
+    #TRACK
     #Inserting Track info with foreign key album_id
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''', 
-        ( name, album_id, length, rating, count ) )
+        (title, album_id, genre_id,len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''', 
+        ( name, album_id, genre_id, length, rating, count ) )
 
     conn.commit()
+#----------------------------------------------------------------------
+#USE THE SQL COMMAND TO CHECK VALIDITY: 
+    # SELECT Track.title, Artist.name, Album.title, Genre.name 
+    # FROM Track JOIN Genre JOIN Album JOIN Artist 
+    # ON Track.genre_id = Genre.ID and Track.album_id = Album.id 
+    #     AND Album.artist_id = Artist.id
+    # ORDER BY Artist.name LIMIT 3
+    
