d2494fc95a21f32caa5d7f5a91cab7fa93577203
Zannatun Nayeem <nayeem123able@gmail.com>
2020-05-14 13:48:53 +0600

    Databases: m to m relationships completed

---
+++ Databases/Week4 (Many-to-Many-Relationships)/Assignment (Many to many)/dbcheck-select-sql.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect('rosterdb.sqlite')
+cur = conn.cursor()
+
+#SQL command to search a query
+cur.execute('''
+SELECT hex(User.name || Course.title || Member.role ) AS X FROM 
+    User JOIN Member JOIN Course 
+    ON User.id = Member.user_id AND Member.course_id = Course.id
+    ORDER BY X                      
+''')
+#Printing out the searched result [0] index 
+print(cur.fetchone()[0])
+
